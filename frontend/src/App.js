6ae52fef46ec535346205e67baa5f7c541fde256
Samarth Ghulyani <samarth@Samarths-MacBook-Air.local>
2023-11-13 18:08:22 -0800

    added register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,29 @@
-import React from 'react';
-import './App.css';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Login from './pages/Login';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Register from "./pages/Register";
 
 const App = () => {
-
   const Menu = () => {
     return (
       <Routes>
-        <Route path='login' element={<Login />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
       </Routes>
-    )
-  }
+    );
+  };
 
   return (
-          <div className='main-app'>
-            <BrowserRouter>
-            <Navbar />
-            <Menu />
-            <Footer />
-            </BrowserRouter>
-          </div>
-        
+    <div className="main-app">
+      <BrowserRouter>
+        <Navbar />
+        <Menu />
+        <Footer />
+      </BrowserRouter>
+    </div>
   );
 };
 
