0efcd3c67109246f046ba1c5a03f1a339895567e
Nagpal Siddhant <siddhantnagpal03@gmail.com>
2023-11-30 15:31:48 -0800

    - Add Theater Occupancy by Movie logic

--- backend/src/main/java/com/scrumandcoke/movietheaterclub/configuration/WebMvcConfig.java
+++ backend/src/main/java/com/scrumandcoke/movietheaterclub/configuration/WebMvcConfig.java
@@ -24,9 +24,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:3000", "https://bee5-2601-646-a100-cbf0-e164-ed22-95cd-68aa.ngrok-free.app")
                 .allowedMethods("*")
-                .allowedHeaders("*");
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
 
