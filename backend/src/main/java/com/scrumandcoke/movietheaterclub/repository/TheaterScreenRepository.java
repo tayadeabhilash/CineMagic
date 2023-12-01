d7c1d45bd0cf5628e52ab4b12efb549f86b1595c
Nagpal Siddhant <siddhantnagpal03@gmail.com>
2023-12-01 04:16:05 -0800

    - Add Theater by Location

--- backend/src/main/java/com/scrumandcoke/movietheaterclub/repository/TheaterScreenRepository.java
+++ backend/src/main/java/com/scrumandcoke/movietheaterclub/repository/TheaterScreenRepository.java
@@ -1,11 +1,14 @@
 package com.scrumandcoke.movietheaterclub.repository;
 
+import com.scrumandcoke.movietheaterclub.entity.TheaterScreenEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.scrumandcoke.movietheaterclub.entity.TheaterScreenEntity;
+import java.util.List;
 
 @Repository
 public interface TheaterScreenRepository extends JpaRepository<TheaterScreenEntity, Integer>{
+
+    List<TheaterScreenEntity> findByLocationEntity_Id(Integer id);
 
 }
